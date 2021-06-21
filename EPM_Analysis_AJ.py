1853483b3289e34b51693a9d7504fee5257eb948
AJU707 <anesju707@gmail.com>
2021-06-21 14:37:56 +0900

    Update EPM_Analysis_AJ.py

--- EPM_Analysis_AJ.py
+++ EPM_Analysis_AJ.py
@@ -1,22 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Video analysis of EPM by Bonsai & Python
+Video analysis of Elevated Plus Maze (EPM) by Bonsai & Python
 
-written by Anes Ju, 20200408
-modified by Yoni Couderc, 25/03/2021
+written by Anes Ju, 2021.06.15
+modified by Anes Ju, 2021.06.21
 
-This code analyze all the csv and its video file  of mice which are written in excel file in the same directory. This is to draw the trajectory of mouse location from csv file analyzed by Bonsai program and calculate the time spent and distance travelled in each arm and center in elevated plus maze (EPM) experiments.
+This script analyzes all the csv and its video files of elevated plus maze experiments from mice which are listed in excel file in the same directory. 
+This script is 
+
+(1) to analyze the time spent (second) and distance travelled (cm) in each arm and center in specific time bin you enter (in second)
+(2) to analyze the time spent (second), distance travelled (cm) and speed (cm/s) in open/closed arms and center for 05 minutes, 10 minutes and 15 minutes (depending on the experimental time)
+(3) to draw the trajectory of each mouse for 05 minutes, 10 minutes and 15 minutes (depending on the experimental time)
 
 *******
 Important information before starting this code
 
-1. In the working directory, you should have a video file (F835_EPM.avi) and its bonsai file (F835_EPM-bonsai.csv) of each animal and an excel file (EPM_Time_Spent.xlsx) with animal no, sex and the starting time (as second) of video analysis. Please keep the file name as above. 
+1. In the same working directory, you should have a video file (F835_EPM.avi) and its bonsai file (F835_EPM-bonsai.csv) of each animal and an excel file (EPM_data.xlsx : Should include information with column names; 'Animal no':int, 'Sex':str,'Starting time':int (in sec), 'Group': str) with animal no, sex and the starting time (as second) of video analysis. Please keep the file name as above. 
 
-2. When the first frame of video pops up, you should click 12 corners of EPM counterclockwise from top left corner of upper closed arm and esc key to close the video.
+2. In the console, there will be three questions poped up: 
+    
+    'Please enter how long one session of the experiment takes (in minutes):   ' --> Please write the experimental time in minutes. Then, press enter key.
+    'Please enter the time bin (in second):   '. --> Please write the divisor of experimental time (in second) that you entered. Then, press enter key.
+    'Do you want to define 5 regions of EPM (press 1) or 3 regions of EPM (press 2)?   ' --> Press 1 or 2. Then, press enter key.
+
+3. When the first frame of video pops up, you should click 12 corners of EPM counterclockwise from the top left corner of upper closed arm and enter esc key to close the video.
+
+4. It is important to click the exact coordinates of center, since the pixel size is calculated based on those coordinates.
+
+5. At the end, you will have an excel file with all the data analyzed and the trajectory of each mouse for 05 minutes, 10 minutes and 15 minutes (depending on the experimental time).
+
+6. If you want to reanalyze, please check whether you have proper column names and remove previous data in excel file.
 *******
-
 Enjoy the analysis!
-
 """
 
 # Import packages
@@ -25,6 +40,7 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 from matplotlib.path import Path
 import matplotlib.patches as patches
+from matplotlib import rc
 import numpy as np
 
 def onclick(event,x,y,flag,image):
@@ -34,10 +50,6 @@ def onclick(event,x,y,flag,image):
     global coords
     if event == cv.EVENT_LBUTTONUP:
         coords.append((x,y))
-        # wait for Esc or q key and then exit
-        # if len(coords) ==12:
-        #     # cv.polylines(img,coords,True, (0,255,255),
-        #     #         lineType=cv.LINE_AA)
         return
 
 def define_ROI(coords):
@@ -52,11 +64,10 @@ def define_ROI(coords):
     coords_list = [open_arm_1, open_arm_2, closed_arm_1, closed_arm_2, center]
     return coords_list
         
-def draw_graph(bonsai_file, coords_list, animal_no, animal_sex, time, width, height):
+def draw_trajectory(bonsai_file, coords_list, animal_no, animal_sex, time, width, height):
     """
     Draw the trajectory of mouse location depending on experimental time and save this graph.
     """   
-
     # Plot each point
     plt.scatter(bonsai_file['mouseX'], bonsai_file['mouseY'], s=0.5)
     
@@ -79,20 +90,23 @@ def draw_graph(bonsai_file, coords_list, animal_no, animal_sex, time, width, hei
     plt.ylabel('Y')
     plt.xlim(0, width)
     plt.ylim(0, height)
+    plt. rcParams["figure.figsize"] = (8,6)
+    rc('font', **{'family':'Arial'})
+    plt.axis('off')
     plt.savefig("%s%s_EPM_%s.jpg"%(animal_sex, animal_no, time), forat = 'jpg')
     plt.show()
     return
 
 def calculate_pixel(coords_list):
     """
-    Based on the coordinates of EPM (60cmx60cm), calculate and return the size of pixel
+    Based on the coordinates of EPM (5cm x 5cm in center), calculate and return the size of pixel
     """
     x_1, y_1 = coords_list[1]
     x_2, y_2 = coords_list[4]
     x_3, y_3 = coords_list[7]
     x_4, y_4 = coords_list[10]
 
-    p_1 = (25/((x_1-x_2)**2 + (y_1-y_2)**2))**0.5 # size of pixel in centimeter. 25 means 5cm x5cm for center of EPM. 
+    p_1 = (25/((x_1-x_2)**2 + (y_1-y_2)**2))**0.5 # size of pixel in centimeter. 25 means 5cmx5cm for center of EPM. 
     p_2 = (25/((x_3-x_2)**2 + (y_3-y_2)**2))**0.5
     p_3 = (25/((x_3-x_4)**2 + (y_3-y_4)**2))**0.5
     p_4 = (25/((x_1-x_4)**2 + (y_1-y_4)**2))**0.5
@@ -104,24 +118,21 @@ def calculate_time(bonsai_file, frame_rate, coords_list):
     """
     Calculate the time spent in ROIs and return the data in list format
     """
-
     bonsai_file_list = list(zip(bonsai_file['mouseX'], bonsai_file['mouseY']))
-    time_spent=[]
-    
+    time_spent=[]    
     for i in range(len(coords_list)):
         inside_region = coords_list[i].contains_points(bonsai_file_list)
         time_inside_region = sum(inside_region)/frame_rate
         time_spent.append(time_inside_region)        
-
     return time_spent
 
-def calculate_distance(bonsai_file, coords_list, p):
+def calculate_distance(bonsai_file, p, coords_list):
     """
-    Calculate distance travelled in ROI and return a list of [dist in OA_left, dist in OA_right, dist in CA_up, dist in CA_down, dist in center,  total dist]. Please take the order of elements into account when you add these values in the data set!!
+    Calculate distance travelled in ROI and return a list of [dist in OA_left, dist in OA_right, dist in CA_up, dist in CA_down, dist in center,  Total dist]. Please take the order of elements into account when you add these values in the data set!!
     """    
     x = bonsai_file['mouseX']
     y = bonsai_file['mouseY']
-    total_dist = []
+    Total_dist = []
     OA_left_dist = []
     OA_right_dist = []
     CA_up_dist = []
@@ -132,7 +143,7 @@ def calculate_distance(bonsai_file, coords_list, p):
         a = i
         b = a+1
         dist = (((x[a] - x[b])*p)**2+((y[a] - y[b])*p)**2)**0.5
-        total_dist.append(dist)
+        Total_dist.append(dist)
 
         if coords_list[0].contains_point((x[a], y[a])):
             OA_left_dist.append(dist)
@@ -144,8 +155,7 @@ def calculate_distance(bonsai_file, coords_list, p):
             CA_down_dist.append(dist)
         elif coords_list[4].contains_point((x[a], y[a])):
             CT_dist.append(dist)
-
-    return [np.nansum(OA_left_dist), np.nansum(OA_right_dist), np.nansum(CA_up_dist), np.nansum(CA_down_dist), np.nansum(CT_dist), np.nansum(total_dist)]        
+    return [np.nansum(OA_left_dist), np.nansum(OA_right_dist), np.nansum(CA_up_dist), np.nansum(CA_down_dist), np.nansum(CT_dist), np.nansum(Total_dist)]        
 
 if __name__ == '__main__':
 
@@ -156,7 +166,33 @@ if __name__ == '__main__':
     # Get all the animal no, sex and starting time as list formats
     animal_sex = df['Sex'].to_list()
     animal_no = df.index.to_list()
-    starting_time = df['Starting time'].to_list()
+    starting_time = df['Starting time'].to_list()    
+        
+    # Ask the time bin in sec and get the value in integer format
+    total_exp_time_min = int(input('Please enter how long one session of the experiment takes (in minutes):   '))
+    total_exp_time_sec = total_exp_time_min*60
+    timebin_in_sec = int(input('Please enter the time bin in sec:  '))    
+    analysis_way = int(input('Do you want to define 5 regions of EPM (press 1) or 3 regions of EPM (press 2)?   '))
+        
+    # Create the column names depending on the time bin    
+    df_columns = ['Timebin']
+    bin_5min = int(total_exp_time_min/5)
+    for period in ['05min', '10min', '15min']:
+        df_columns = df_columns + ['Time_OA_%s'%(period), 'Time_CA_%s'%(period), 'Time_CT_%s'%(period), 'Dist_OA_%s'%(period), 'Dist_CA_%s'%(period), 'Dist_CT_%s'%(period), 'Dist_Total_%s'%(period), 'Speed_OA_%s'%(period), 'Speed_CA_%s'%(period), 'Speed_CT_%s'%(period), 'Speed_Total_%s'%(period)]
+    
+    for i in range(int(total_exp_time_sec/timebin_in_sec)):
+        start = timebin_in_sec * i
+        end = timebin_in_sec * (i+1)
+        period = '%ss_%ss'%(str(start), str(end))   
+        if analysis_way == 2:
+            df_columns = df_columns + ['Time_CT_%s'%(period), 'Time_OA_%s'%(period), 'Time_CA_%s'%(period), 'Dist_CT_%s'%(period), 'Dist_Total_%s'%(period), 'Dist_OA_%s'%(period), 'Dist_CA_%s'%(period)]                       
+        elif analysis_way == 1:     
+            df_columns = df_columns + ['Time_OA_left_%s'%(period), 'Time_OA_right_%s'%(period), 'Time_CA_up_%s'%(period), 'Time_CA_down_%s'%(period), 'Time_CT_%s'%(period), 'Time_OA_%s'%(period), 'Time_CA_%s'%(period), 'Dist_OA_left_%s'%(period), 'Dist_OA_right_%s'%(period), 'Dist_CA_up_%s'%(period), 'Dist_CA_down_%s'%(period), 'Dist_CT_%s'%(period), 'Dist_Total_%s'%(period), 'Dist_OA_%s'%(period), 'Dist_CA_%s'%(period)]        
+
+    # Create the new columns with new column names in the dataframe
+    df_add = pd.DataFrame(index = animal_no, columns = df_columns)
+    df_add = df_add.fillna(0)
+    df = pd.concat([df, df_add], axis=1)
     
     # Open the video file and its bonsai file of each mouse from excel file
     for no, sex, time in zip(animal_no, animal_sex, starting_time):
@@ -189,97 +225,82 @@ if __name__ == '__main__':
         while True:
             key = cv.waitKey(1) & 0xFF
             if key == 27 or key == ord("q"):
-                print('Image cropped at coordinates: {}'.format(coords))
                 cv.destroyAllWindows()
                 break             
-
-        coords_list=define_ROI(coords)
+        
+        # Calculate the size of pixel (centimeter for EPM)
+        coords_list = define_ROI(coords)
+        p = calculate_pixel(coords)
         
         # Open the bonsai file in csv format
-        bonsai_file = pd.read_csv(r'%s-EPM-bonsai.csv'%(anim_id),sep='\s+', engine='python', encoding = "cp949", converters={'mouseX': float,'mouseY': float,'mouseAngle': float,'mouseMajorAxisLength': float, 'mouseMinorAxisLength': float,'mouseArea': float})  
+        bonsai_file = pd.read_csv(r'%s_EPM-bonsai.txt'%(anim_id),sep='\s+', engine='python', encoding = "cp949", converters={'mouseX': float,'mouseY': float,'mouseAngle': float,'mouseMajorAxisLength': float, 'mouseMinorAxisLength': float,'mouseArea': float})  
         
-        # Get the starting time and calculate the ending time        
-        starting_frame = int(time*frame_rate)
-        ending_frame_15min = int((time+60*15)*frame_rate)
-        ending_frame_10min = int((time+60*10)*frame_rate)
-        ending_frame_05min = int((time+60*5)*frame_rate)
+        # Calculate the time spent and the distance travelled in each region by specific time bin
+        for i in range(int(total_exp_time_sec/(timebin_in_sec))):
+            start = timebin_in_sec * i
+            end = timebin_in_sec * (i+1)
+            
+            a = int(frame_rate * (time + start))
+            b = int(frame_rate * (time + end))
+            bonsai_file_timebin = bonsai_file.iloc[a:b]
+            bonsai_file_timebin.columns = bonsai_file.columns
+            df_time_timebin = calculate_time(bonsai_file_timebin, frame_rate, coords_list)
+            df_dist_timebin = calculate_distance(bonsai_file_timebin, p, coords_list)
+            
+            period = '%ss_%ss'%(str(start), str(end))
+            print(period)
+            
+            df['Time_CT_%s'%(period)][no] = df_time_timebin[4]
+            df['Time_OA_%s'%(period)][no] = df_time_timebin[0] + df_time_timebin[1]
+            df['Time_CA_%s'%(period)][no] = df_time_timebin[2] + df_time_timebin[3]            
+    
+            df['Dist_CT_%s'%(period)][no] = df_dist_timebin[4]
+            df['Dist_Total_%s'%(period)][no] = df_dist_timebin[5]
+            df['Dist_OA_%s'%(period)][no] = df_dist_timebin[0] + df_dist_timebin[1]
+            df['Dist_CA_%s'%(period)][no] = df_dist_timebin[2] + df_dist_timebin[3]            
+                            
+            if analysis_way == 1:    
+                df['Time_OA_left_%s'%(period)][no] = df_time_timebin[0]
+                df['Time_OA_right_%s'%(period)][no] = df_time_timebin[1]
+                df['Time_CA_up_%s'%(period)][no] = df_time_timebin[2]
+                df['Time_CA_down_%s'%(period)][no] = df_time_timebin[3]
                 
-        # Calculate the size of pixel (centimeter for EPM)
-        p = calculate_pixel(coords)
-        print(p)
-        
-        # Data for 15 mins        
-        bonsai_file_15min = bonsai_file.iloc[starting_frame:ending_frame_15min]
-        bonsai_file_15min.columns = bonsai_file.columns
-        df_time_15min = calculate_time(bonsai_file_15min, frame_rate, coords_list)
-        df_dist_15min = calculate_distance(bonsai_file_15min, coords_list, p)
-        draw_graph(bonsai_file_15min, coords_list, anim_no, sex, '15min', width, height)
-    
-        df['Time_OA_left_15min'][no] = df_time_15min[0]
-        df['Time_OA_right_15min'][no] = df_time_15min[1]
-        df['Time_CA_up_15min'][no] = df_time_15min[2]
-        df['Time_CA_down_15min'][no] = df_time_15min[3]
-        df['Time_CT_15min'][no] = df_time_15min[4]
-        df['Time_OA_15min'][no] = df_time_15min[0] + df_time_15min[1]
-        df['Time_CA_15min'][no] = df_time_15min[2] + df_time_15min[3]
-        
-        df['Dist_OA_left_15min'][no] = df_dist_15min[0]
-        df['Dist_OA_right_15min'][no] = df_dist_15min[1]
-        df['Dist_CA_up_15min'][no] = df_dist_15min[2]
-        df['Dist_CA_down_15min'][no] = df_dist_15min[3]
-        df['Dist_CT_15min'][no] = df_dist_15min[4]
-        df['Dist_total_15min'][no] = df_dist_15min[5]
-        df['Dist_OA_15min'][no] = df_dist_15min[0] + df_dist_15min[1]
-        df['Dist_CA_15min'][no] = df_dist_15min[2] + df_dist_15min[3]
-        
-         # Data for 10 mins        
-        bonsai_file_10min = bonsai_file.iloc[starting_frame:ending_frame_10min]
-        bonsai_file_10min.columns = bonsai_file.columns
-        df_time_10min = calculate_time(bonsai_file_10min, frame_rate, coords_list)
-        df_dist_10min = calculate_distance(bonsai_file_10min, coords_list, p)
-        draw_graph(bonsai_file_10min, coords_list, anim_no, sex, '10min', width, height)
-    
-        df['Time_OA_left_10min'][no] = df_time_10min[0]
-        df['Time_OA_right_10min'][no] = df_time_10min[1]
-        df['Time_CA_up_10min'][no] = df_time_10min[2]
-        df['Time_CA_down_10min'][no] = df_time_10min[3]
-        df['Time_CT_10min'][no] = df_time_10min[4]
-        df['Time_OA_10min'][no] = df_time_10min[0] + df_time_10min[1]
-        df['Time_CA_10min'][no] = df_time_10min[2] + df_time_10min[3]
-        
-        df['Dist_OA_left_10min'][no] = df_dist_10min[0]
-        df['Dist_OA_right_10min'][no] = df_dist_10min[1]
-        df['Dist_CA_up_10min'][no] = df_dist_10min[2]
-        df['Dist_CA_down_10min'][no] = df_dist_10min[3]
-        df['Dist_CT_10min'][no] = df_dist_10min[4]
-        df['Dist_total_10min'][no] = df_dist_10min[5]
-        df['Dist_OA_10min'][no] = df_dist_10min[0] + df_dist_10min[1]
-        df['Dist_CA_10min'][no] = df_dist_10min[2] + df_dist_10min[3]
-        
-        # Data for 05 mins        
-        bonsai_file_05min = bonsai_file.iloc[starting_frame:ending_frame_05min]
-        bonsai_file_05min.columns = bonsai_file.columns
-        df_time_05min = calculate_time(bonsai_file_05min, frame_rate, coords_list)
-        df_dist_05min = calculate_distance(bonsai_file_05min, coords_list, p)
-        draw_graph(bonsai_file_05min, coords_list, anim_no, sex, '05min', width, height)
-    
-        df['Time_OA_left_05min'][no] = df_time_05min[0]
-        df['Time_OA_right_05min'][no] = df_time_05min[1]
-        df['Time_CA_up_05min'][no] = df_time_05min[2]
-        df['Time_CA_down_05min'][no] = df_time_05min[3]
-        df['Time_CT_05min'][no] = df_time_05min[4]
-        df['Time_OA_05min'][no] = df_time_05min[0] + df_time_05min[1]
-        df['Time_CA_05min'][no] = df_time_05min[2] + df_time_05min[3]
-        
-        df['Dist_OA_left_05min'][no] = df_dist_05min[0]
-        df['Dist_OA_right_05min'][no] = df_dist_05min[1]
-        df['Dist_CA_up_05min'][no] = df_dist_05min[2]
-        df['Dist_CA_down_05min'][no] = df_dist_05min[3]
-        df['Dist_CT_05min'][no] = df_dist_05min[4]
-        df['Dist_total_05min'][no] = df_dist_05min[5]
-        df['Dist_OA_05min'][no] = df_dist_05min[0] + df_dist_05min[1]
-        df['Dist_CA_05min'][no] = df_dist_05min[2] + df_dist_05min[3]
+                df['Dist_OA_left_%s'%(period)][no] = df_dist_timebin[0]
+                df['Dist_OA_right_%s'%(period)][no] = df_dist_timebin[1]
+                df['Dist_CA_up_%s'%(period)][no] = df_dist_timebin[2]
+                df['Dist_CA_down_%s'%(period)][no] = df_dist_timebin[3]
+            
+        # Draw the trajectory of each mouse for 05, 10 and 15 minutes
+        for i in range(bin_5min):
+            i = i+1
+            a = int(frame_rate*time)
+            b = int(frame_rate*(time+i*300))
+            print(i)
+            
+            bonsai_file_new = bonsai_file.iloc[a:b]
+            bonsai_file_new.columns = bonsai_file.columns
+            df_time = calculate_time(bonsai_file_new, frame_rate, coords_list)
+            df_dist = calculate_distance(bonsai_file_new, p, coords_list)
+            time_str = str('%02d'%(i*5))+'min'
 
-        df_write = pd.ExcelWriter(excel_file, engine='xlsxwriter')
-        df.to_excel(df_write, sheet_name = 'Sheet1')
-        df_write.save()
+            df['Time_CT_%s'%(time_str)][no] = df_time[4]
+            df['Time_OA_%s'%(time_str)][no] = df_time[0] + df_time[1]
+            df['Time_CA_%s'%(time_str)][no] = df_time[2] + df_time[3]
+            
+            df['Dist_CT_%s'%(time_str)][no] = df_dist[4]
+            df['Dist_Total_%s'%(time_str)][no] = df_dist[5]
+            df['Dist_OA_%s'%(time_str)][no] = df_dist[0] + df_dist[1]
+            df['Dist_CA_%s'%(time_str)][no] = df_dist[2] + df_dist[3]
+    
+            df['Speed_CT_%s'%(time_str)][no] = float(df_dist[4])/float(df_time[4])
+            df['Speed_Total_%s'%(time_str)][no] = float(df_dist[5])/(i*300)
+            df['Speed_OA_%s'%(time_str)][no] = float((df_dist[0] + df_dist[1]))/float((df_time[0] + df_time[1]))
+            df['Speed_CA_%s'%(time_str)][no] = float((df_dist[2] + df_dist[3]))/float((df_time[2] + df_time[3]))
+        
+            draw_trajectory(bonsai_file_new, coords_list, anim_no, sex, time_str, width, height)                   
+        
+        df['Timebin'][no] = timebin_in_sec
+    # Save all the data in excel file back
+    df_write = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+    df.to_excel(df_write, sheet_name = 'Sheet1')
+    df_write.save()
